8311d8f4e9757e8012f81050ec22c69c66503eee
abby rimbey <arimbey@novetta.com>
2018-06-20 17:41:37 +0000

    Simplify cot script

--- devScripts/cot_tmux.sh
+++ devScripts/cot_tmux.sh
@@ -11,33 +11,22 @@ then
 
  #Pane 0
  #First Window cot generator
- tmux send-keys -t ${SESSION_NAME} 'ares && aresdev' C-m
- tmux send-keys -t ${SESSION_NAME} 'cd lib/generator/cot_generator' C-m
- tmux send-keys -t ${SESSION_NAME} 'go run main.go -outIp 234.1.2.3:1234 -num 250' C-m
+ tmux send-keys -t ${SESSION_NAME} 'rungen' C-m
 
  #Pane 2
  tmux split-window -h -t ${SESSION_NAME}
- tmux send-keys -t ${SESSION_NAME} 'ares && aresdev' C-m
- tmux send-keys -t ${SESSION_NAME} 'cd lib/geosub/geosubscriptions' C-m
- tmux send-keys -t ${SESSION_NAME} 'go run main.go' C-m
+ tmux send-keys -t ${SESSION_NAME} 'rungeosub' C-m
 
  #Pane 3
  tmux split-window -v -t ${SESSION_NAME}
- tmux send-keys -t ${SESSION_NAME} 'ares && aresdev' C-m
- tmux send-keys -t ${SESSION_NAME} 'cd lib/recorder' C-m
- tmux send-keys -t ${SESSION_NAME} 'go run recorder.go' C-m
+ tmux send-keys -t ${SESSION_NAME} 'runrecorder' C-m
 
  #Pane 1
  tmux select-pane -L -t ${SESSION_NAME}
  tmux split-window -v -t ${SESSION_NAME}
- tmux send-keys -t ${SESSION_NAME} 'nov && cd executor/distexecutorservice' C-m
- tmux send-keys -t ${SESSION_NAME} 'aresdev' C-m
- tmux send-keys -t ${SESSION_NAME} 'go run main.go' C-m
+ tmux send-keys -t ${SESSION_NAME} 'runexec' C-m
 
- tmux attach -t ${SESSION_NAME}
 else
- tmux attach -t ${SESSION_NAME}
-
  tmux kill-pane -t ${SESSION_NAME}
  tmux kill-pane -t ${SESSION_NAME}
  tmux kill-pane -t ${SESSION_NAME}
@@ -45,30 +34,22 @@ else
 
  # Create the session
  tmux new-session -s ${SESSION_NAME} -n generator -d
-
- #Pane 0
+#Pane 0
  #First Window cot generator
- tmux send-keys -t ${SESSION_NAME} 'ares && aresdev' C-m
- tmux send-keys -t ${SESSION_NAME} 'cd lib/generator/cot_generator' C-m
- tmux send-keys -t ${SESSION_NAME} 'go run main.go -outIp 234.1.2.3:1234 -num 250' C-m
+ tmux send-keys -t ${SESSION_NAME} 'rungen' C-m
 
  #Pane 2
  tmux split-window -h -t ${SESSION_NAME}
- tmux send-keys -t ${SESSION_NAME} 'ares && aresdev' C-m
- tmux send-keys -t ${SESSION_NAME} 'cd lib/geosub/geosubscriptions' C-m
- tmux send-keys -t ${SESSION_NAME} 'go run main.go' C-m
+ tmux send-keys -t ${SESSION_NAME} 'rungeosub' C-m
 
  #Pane 3
  tmux split-window -v -t ${SESSION_NAME}
- tmux send-keys -t ${SESSION_NAME} 'ares && aresdev' C-m
- tmux send-keys -t ${SESSION_NAME} 'cd lib/recorder' C-m
- tmux send-keys -t ${SESSION_NAME} 'go run recorder.go' C-m
+ tmux send-keys -t ${SESSION_NAME} 'runrecorder' C-m
 
  #Pane 1
  tmux select-pane -L -t ${SESSION_NAME}
  tmux split-window -v -t ${SESSION_NAME}
- tmux send-keys -t ${SESSION_NAME} 'nov && cd executor/distexecutorservice' C-m
- tmux send-keys -t ${SESSION_NAME} 'aresdev' C-m
- tmux send-keys -t ${SESSION_NAME} 'go run main.go' C-m
-
+ tmux send-keys -t ${SESSION_NAME} 'runexec' C-m
 fi
+
+tmux attach -t ${SESSION_NAME}
